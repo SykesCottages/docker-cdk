006d893d2e72a28d3ebbd2240022ec0e5ed6aabb
James Lockhart <james.lockhart@sykescottages.co.uk>
2024-01-09 09:49:18 +0000

    Fixed the base and latest manifest builds

--- manifest.sh
+++ manifest.sh
@@ -6,6 +6,7 @@ LATEST_VERSION="2"
 docker login >>/dev/null 2>&1
 
 TAG="sykescottages/cdk:base"
+docker manifest rm $TAG
 docker manifest create $TAG \
   --amend "$TAG-amd64" \
   --amend "$TAG-arm64"
@@ -23,6 +24,7 @@ done
 
 TAG="sykescottages/cdk:latest"
 LATEST_TAG="sykescottages/cdk:$LATEST_VERSION"
+docker manifest rm $TAG
 docker manifest create $TAG \
   --amend "$LATEST_TAG-amd64" \
   --amend "$LATEST_TAG-arm64"
